77880763af07cfe7607e1fe0f5eb1a6f8cde7f46
Mallory Spector <malloryspector@gmail.com>
2015-10-17 18:35:04 -0400

    Working random user generator and form validation

--- app/Http/Controllers/RandomUserController.php
+++ app/Http/Controllers/RandomUserController.php
@@ -3,6 +3,7 @@
 namespace DevTools\Http\Controllers;
 
 use DevTools\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class RandomUserController extends Controller {
 
@@ -20,7 +21,40 @@ class RandomUserController extends Controller {
   /**
   * Responds to requests to POST /random-user-generator
   */
-  public function postIndex() {
-    return view('randomuser');
+  public function postIndex(Request $request) {
+    // validate data
+    $this->validate($request, [
+      'number_of_users' => 'required|numeric|max:9',
+    ]);
+
+    $faker = \Faker\Factory::create();
+
+    // collect all form values
+    $number_of_users = $request->input('number_of_users');
+    $name = $request->input('name');
+    $address = $request->input('address');
+    $email = $request->input('email');
+    $phone_number = $request->input('phone_number');
+
+    // create an empty array to hold user information
+    $user_info = array();
+
+    // create user content based on number of users requested and form values
+    for ($i = 0; $i < $number_of_users; $i++) {
+      if (isset($name)) {
+        $user_info[$i]['name'] = $faker->firstName . ' ' . $faker->lastName;
+      }
+      if (isset($address)) {
+        $user_info[$i]['address'] = $faker->address;
+      }
+      if (isset($email)) {
+        $user_info[$i]['email'] = $faker->email;
+      }
+      if (isset($phone_number)) {
+        $user_info[$i]['phone_number'] = $faker->phoneNumber;
+      }
+    }
+
+    return view('randomuser')->with('user_info', $user_info);
   }
 }
--- resources/views/randomuser.blade.php
+++ resources/views/randomuser.blade.php
@@ -15,13 +15,45 @@
     <form method="POST" action="/random-user-generator">
       <input type='hidden' name='_token' value='{{ csrf_token() }}'>
       <div class="form-group">
-        <input type="checkbox" id="first_name">
-        <label for="first_name">First Name</label>
+        {{-- Number of Users --}}
+        <label for="number_of_users">Number of Users</label>
+        <input type="text" class="form-control" id="number_of_users" name="number_of_users" placeholder="Number of Users">
+      </div>
+      <div class="form-group">
+        {{-- Name --}}
+        <input type="checkbox" id="name" name="name">
+        <label for="name">Name</label><br>
+        {{-- Address --}}
+        <input type="checkbox" id="address" name="address">
+        <label for="address">Address</label><br>
+        {{-- Email --}}
+        <input type="checkbox" id="email" name="email">
+        <label for="email">E-Mail</label><br>
+        {{-- Phone Number --}}
+        <input type="checkbox" id="phone_number" name="phone_number">
+        <label for="phone_number">Phone Number</label>
       </div>
       <button type="submit" class="btn btn-default">Submit</button>
     </form>
   </div>
   <div class="col-md-6">
     <h2>Output</h2>
+    @if (isset($user_info))
+      @foreach ($user_info as $user => $information)
+        @if (isset($information['name']))
+          {{ $information['name'] }}<br>
+        @endif
+        @if (isset($information['address']))
+          {{ $information['address'] }}<br>
+        @endif
+        @if (isset($information['email']))
+          {{ $information['email'] }}<br>
+        @endif
+        @if (isset($information['phone_number']))
+          {{ $information['phone_number'] }}<br>
+        @endif
+        <br>
+      @endforeach
+    @endif
   </div>
 @stop
