45943c892b673c4d7818eb5f34a324587fe084f5
Mallory Spector <malloryspector@gmail.com>
2015-10-17 21:58:18 -0400

    Convert password generator to laravel with form validation

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -3,6 +3,7 @@
 namespace DevTools\Http\Controllers;
 
 use DevTools\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class PasswordController extends Controller {
 
@@ -20,7 +21,62 @@ class PasswordController extends Controller {
   /**
   * Responds to requests to POST /xkcd-password-generator
   */
-  public function postIndex() {
-    return view('xkcdpassword');
+  public function postIndex(Request $request) {
+    // validate data
+    $this->validate($request, [
+      'number_of_words' => 'required|numeric|max:9',
+    ]);
+
+    // to do - move these to storage
+    $words = array("notice", "yarn", "want", "second", "cat", "impolite", "pump", "playground", "blue", "box", "day", "produce", "table", "sheet", "apparatus", "protect", "late", "house", "lumpy", "wooden", "banana", "balloon", "dog", "index", "receipt", "proposal", "dear", "faint", "song", "big", "impact", "crowd", "silk", "poem", "define", "budget", "cow", "chicken", "crime", "stock", "arrival", "high", "portrait", "police", "afford");
+    $symbols = array("!", "@", "#", "$", "%", "?", "&", "*");
+    //$file = file_get_contents(storage_path() . ‘/app/words/words.txt’);
+
+    $number_of_words = $request->input('number_of_words');
+    $add_a_number = $request->input('add_a_number');
+    $additional_number_qty = $request->input('additional_numbers');
+    $add_a_symbol = $request->input('add_a_symbol');
+    $additional_symbol_qty = $request->input('additional_symbols');
+    $separator = $request->input('separator');
+
+
+    $password_words = array();
+
+    for ($i = 0; $i < $number_of_words; $i++) {
+      $rand_word = $words[rand(0, count($words)-1)];
+      $password_words[$i] = $rand_word;
+    }
+
+    if (isset($add_a_number)) {
+      for ($i = 1; $i <= $additional_number_qty; $i++) {
+        $add_number = rand(1, 9);
+        $num_place = rand(0, count($password_words)-1);
+        $password_words[$num_place] = $password_words[$num_place] . $add_number;
+      }
+    }
+
+    if (isset($add_a_symbol)) {
+      for ($i = 1; $i <= $additional_symbol_qty; $i++) {
+        $add_symbol = $symbols[rand(0, count($symbols)-1)];
+        $sym_place = rand(0, count($password_words)-1);
+        $password_words[$sym_place] = $password_words[$sym_place] . $add_symbol;
+      }
+    }
+
+    if (isset($separator)) {
+      if ($separator == "hyphen") {
+        $separator = "-";
+      } elseif ($separator == "space") {
+        $separator = " ";
+      } elseif ($separator == "no space") {
+        $separator = "";
+      } elseif ($separator == "dot") {
+        $separator = ".";
+      }
+    }
+
+    $password = implode($separator, $password_words);
+
+    return view('xkcdpassword')->with('password', $password);
   }
 }
--- resources/views/xkcdpassword.blade.php
+++ resources/views/xkcdpassword.blade.php
@@ -13,17 +13,17 @@
   <div class="col-md-6">
     <h2>Form</h2>
     <form method="POST" action="/xkcd-password-generator">
-      <input type='hidden' name='_token' value='{{ csrf_token() }}'>
+      <input type='hidden' value='{{ csrf_token() }}' name='_token'>
       <div class="form-group">
         {{-- Word Count Section --}}
         <label for="number_of_words">Number of Words</label>
-        <input type="text" class="form-control" id="number_of_words" placeholder="Number of Words (max 9)">
+        <input type="text" class="form-control" id="number_of_words" name="number_of_words" placeholder="Number of Words (max 9)">
       </div>
       <div class="form-group">
         {{-- Number Option Section --}}
-        <input type="checkbox" id="add_a_number">
+        <input type="checkbox" id="add_a_number" name="add_a_number">
         <label for="add_a_number">Add a number</label>
-        <select class="form-control" id="additional_numbers">
+        <select class="form-control" id="additional_numbers" name="additional_numbers">
           <option>1</option>
           <option>2</option>
           <option>3</option>
@@ -34,9 +34,9 @@
       </div>
       <div class="form-group">
         {{-- Symbol Option Section --}}
-        <input type="checkbox" id="add_a_symbol">
+        <input type="checkbox" id="add_a_symbol" name="add_a_symbol">
         <label for="add_a_symbol">Add a symbol</label>
-        <select class="form-control" id="additional_symbols">
+        <select class="form-control" id="additional_symbols" name="additional_symbols">
           <option>1</option>
           <option>2</option>
           <option>3</option>
@@ -48,7 +48,7 @@
       <div class="form-group">
         {{-- Separator Section --}}
         <label for="separator">Choose your separator</label>
-        <select class="form-control" id="separator">
+        <select class="form-control" id="separator" name="separator">
           <option>hyphen</option>
           <option>space</option>
           <option>no space</option>
@@ -60,5 +60,8 @@
   </div>
   <div class="col-md-6">
     <h2>Output</h2>
+    @if (isset($password))
+      <p>{{ $password }}</p>
+    @endif
   </div>
 @stop
